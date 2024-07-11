266402a79f07be8bd25a5b9d75083591cbdbeba9
Ashir Mustafa <100285531+ashirmustafa@users.noreply.github.com>
2024-07-11 10:36:17 +0500

    custom tab nav completed

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -12,22 +12,20 @@ const Layout = () => {
                 tabBarShowLabel: false,
                 tabBarStyle: {
                     position: 'absolute',
-                    bottom: 27,
-                    left: 16,
-                    right: 16,
+                    bottom: 0,
                     height: 72,
                     elevation: 0,
                     backgroundColor: 'white',
-                    borderRadius: 16,
                     alignItems: 'center',
                     justifyContent: 'center',
+                    paddingBottom: 5
                 }
             }}
         >
             <Tabs.Screen name='home' options={{
                 tabBarIcon: ({ focused }) => (
                     <View className="items-center pt-0">
-                        <Ionicons name={focused ? "home" : 'home-outline'} color={focused ? 'red' : 'gray'} size={25} />
+                        <Ionicons name={focused ? "home" : 'home-outline'} color={focused ? '#EB3030' : 'black'} size={25} />
                         <Text style={{ color: focused ? 'red' : 'gray' }}>Home</Text>
                     </View>
                 )
@@ -35,31 +33,31 @@ const Layout = () => {
             <Tabs.Screen name='wishList' options={{
                 tabBarIcon: ({ focused }) => (
                     <View className="items-center pt-0">
-                        <Ionicons name="heart" color={focused ? 'red' : 'gray'} size={25} />
+                        <Ionicons name="heart" color={focused ? '#EB3030' : 'black'} size={25} />
                         <Text style={{ color: focused ? 'red' : 'gray' }}>Wishlist</Text>
                     </View>
                 )
             }} />
             <Tabs.Screen name='cart' options={{
                 tabBarIcon: ({ focused }) => (
-                    <View className={`items-center p-3 rounded-full justify-center shadow-2xl absolute -top-3 ${focused ? 'bg-red-400' : 'bg-white'}`}>
-                        <Ionicons name="cart" color={focused ? 'white' : 'gray'} size={25} />
+                    <View className={`items-center p-4 rounded-full justify-center shadow-2xl absolute -top-3 ${focused ? 'bg-[#EB3030]' : 'bg-white'}`} style={styles.shadowDrop}>
+                        <Ionicons name="cart" color={focused ? 'white' : 'black'} size={25} />
                     </View>
                 )
             }} />
             <Tabs.Screen name='search' options={{
                 tabBarIcon: ({ focused }) => (
                     <View className="items-center pt-0" >
-                        <Ionicons name="search" color={focused ? 'red' : 'gray'} size={25} />
-                        <Text style={{ color: focused ? 'red' : 'gray' }}>Search</Text>
+                        <Ionicons name="search" color={focused ? '#EB3030' : 'black'} size={25} />
+                        <Text style={{ color: focused ? '#EB3030' : 'black' }}>Search</Text>
                     </View>
                 )
             }} />
             <Tabs.Screen name='settings' options={{
                 tabBarIcon: ({ focused }) => (
                     <View className="items-center pt-0">
-                        <Ionicons name="settings" color={focused ? 'red' : 'gray'} size={25} />
-                        <Text style={{ color: focused ? 'red' : 'gray' }}>Settings</Text>
+                        <Ionicons name="settings" color={focused ? '#EB3030' : 'black'} size={25} />
+                        <Text style={{ color: focused ? '#EB3030' : 'black' }}>Settings</Text>
                     </View>
                 )
             }} />
@@ -68,6 +66,14 @@ const Layout = () => {
     );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    shadowDrop: {
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 14,
+        elevation: 2,
+    }
+})
 
 export default Layout;
